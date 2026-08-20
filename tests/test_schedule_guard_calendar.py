e1f5b5ed283d322d80867889a1776805a3aa1c91
ghzyzttxwd <2697941746@qq.com>
2026-08-20 19:12:07 +0800

    test: cover previous exchange session resolution

--- tests/test_schedule_guard_calendar.py
+++ tests/test_schedule_guard_calendar.py
@@ -2,6 +2,7 @@ import unittest
 
 import pandas as pd
 
+from engine.exchange_calendar import exchange_calendar_previous_session
 from engine.schedule_guard import exchange_calendar_latest_session
 
 
@@ -12,6 +13,9 @@ class _FakeAk:
     def tool_trade_date_hist_sina(self):
         return pd.DataFrame({'trade_date': self._dates})
 
+    def stock_zh_index_daily_tx(self, **_kwargs):
+        raise AssertionError('previous-session resolution must not consult Tencent index daily')
+
 
 class ScheduleGuardCalendarTests(unittest.TestCase):
     def test_requested_session_wins_even_if_quotes_could_lag(self):
@@ -20,6 +24,11 @@ class ScheduleGuardCalendarTests(unittest.TestCase):
             exchange_calendar_latest_session('2026-08-20', ak),
             '2026-08-20',
         )
+
+    def test_previous_session_uses_calendar_not_tencent_index(self):
+        ak = _FakeAk(['2026-08-14', '2026-08-17', '2026-08-18', '2026-08-19', '2026-08-20'])
+        self.assertEqual(exchange_calendar_previous_session('2026-08-20', ak), '2026-08-19')
+        self.assertEqual(exchange_calendar_previous_session('2026-08-17', ak), '2026-08-14')
 
     def test_weekday_holiday_resolves_previous_exchange_session(self):
         ak = _FakeAk(['2026-09-30', '2026-10-09'])
